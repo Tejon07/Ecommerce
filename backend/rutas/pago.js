0fd6dd475a91f258883658538e0c0d89a72c29a3
Tejon07 <rolandoquispellojlla1003@gmail.com>
2025-05-30 21:59:40 -0400

    Reinicio del Backend

--- backend/rutas/pago.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const autenticarJWT = require('../middleware/autenticarJWT');
-const { generarQr } = require('../controladores/pagoControlador');
-
-// Genera un QR (falso por ahora)
-router.get('/qr', autenticarJWT, generarQr);
-
-module.exports = router;
